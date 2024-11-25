8b20091fc4d88dab1a5497181e2d5737b9c74304
huynguyen <huynguyen@192.168.1.14>
2024-11-25 16:14:27 +0700

    1122

--- bakery/src/main/java/com/DB/DBConnect.java
+++ bakery/src/main/java/com/DB/DBConnect.java
@@ -7,7 +7,7 @@ public class DBConnect {
 
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/bakery", "root", "Thanhlong@2004.nt");
+			conn= DriverManager.getConnection("jdbc:mysql://localhost:3306/bakery", "root", "huy26012004");
 			
 		} catch(Exception e) {
 			e.printStackTrace();
